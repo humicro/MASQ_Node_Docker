8c5137d935dcb7141855fb47cff2aac768b64e37
microoo <hu@microoo.net>
2021-03-07 06:23:23 +0000

    make use of docker ENV and shell var to simplify scripts

--- compile-linux/compile-linux.sh
+++ compile-linux/compile-linux.sh
@@ -4,23 +4,24 @@ sudo apt-get -y upgrade
 # Update Rust
 rustup update stable
 # Create build directory
-mkdir -p ~/builds/linux-x86-64
-rm -r ~/builds/linux-x86-64/*
+BUILD_TARGET_DIR=~/builds/linux-x86-64
+mkdir -p $BUILD_TARGET_DIR
+rm -r ${BUILD_TARGET_DIR}/*
 # Download MASQ Node source
 git clone --depth 1 https://github.com/MASQ-Project/Node.git src
 # Compile MASQNode
 cd ~/src/node
 cargo build --release --verbose
-cp ~/src/node/target/release/MASQNode ~/builds/linux-x86-64/
+cp ~/src/node/target/release/MASQNode ${BUILD_TARGET_DIR}/
 # Compile masq
 cd ~/src/masq
 cargo build --release --verbose
-cp ~/src/node/target/release/masq ~/builds/linux-x86-64/
+cp ~/src/node/target/release/masq ${BUILD_TARGET_DIR}/
 # Compile dns_utility
 cd ~/src/dns_utility
 cargo build --release --verbose
-cp ~/src/dns_utility/target/release/dns_utility ~/builds/linux-x86-64/
+cp ~/src/dns_utility/target/release/dns_utility ${BUILD_TARGET_DIR}/
 # Compile port_exposer
 cd ~/src/port_exposer
 cargo build --release --verbose
-cp ~/src/port_exposer/target/release/port_exposer ~/builds/linux-x86-64/
+cp ~/src/port_exposer/target/release/port_exposer ${BUILD_TARGET_DIR}/
--- compile-pi/compile-pi.sh
+++ compile-pi/compile-pi.sh
@@ -4,23 +4,24 @@ sudo apt-get -y upgrade
 # Update Rust
 rustup update stable
 # Create build directory
-mkdir -p ~/builds/rpi-arm6
-rm -r ~/builds/rpi-arm6/*
+BUILD_TARGET_DIR=~/builds/rpi-arm6
+mkdir -p $BUILD_TARGET_DIR
+rm -r ${BUILD_TARGET_DIR}/*
 # Download MASQ Node source
 git clone --depth 1 https://github.com/MASQ-Project/Node.git src
 # Compile MASQNode
 cd ~/src/node
 cargo build --release --verbose
-cp ~/src/node/target/arm-unknown-linux-gnueabihf/release/MASQNode ~/builds/rpi-arm6/
+cp ~/src/node/target/${RUST_CROSS_TARGET}/release/MASQNode ${BUILD_TARGET_DIR}/
 # Compile masq
 cd ~/src/masq
 cargo build --release --verbose
-cp ~/src/node/target/arm-unknown-linux-gnueabihf/release/masq ~/builds/rpi-arm6/
+cp ~/src/node/target/${RUST_CROSS_TARGET}/release/masq ${BUILD_TARGET_DIR}/
 # Compile dns_utility
 cd ~/src/dns_utility
 cargo build --release --verbose
-cp ~/src/dns_utility/target/arm-unknown-linux-gnueabihf/release/dns_utility ~/builds/rpi-arm6/
+cp ~/src/dns_utility/target/${RUST_CROSS_TARGET}/release/dns_utility ${BUILD_TARGET_DIR}/
 # Compile port_exposer
 cd ~/src/port_exposer
 cargo build --release --verbose
-cp ~/src/port_exposer/target/arm-unknown-linux-gnueabihf/release/port_exposer ~/builds/rpi-arm6/
+cp ~/src/port_exposer/target/${RUST_CROSS_TARGET}/release/port_exposer ${BUILD_TARGET_DIR}/
