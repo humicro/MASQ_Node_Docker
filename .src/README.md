9e2f9145356960a251787ae2bfd6bd5c0d3f8ad4
microoo <hu@microoo.net>
2021-03-15 19:35:37 +0000

    locally persist repo

---
+++ .src/README.md
@@ -0,0 +1,5 @@
+# Local Copy of MASQ Repositories
+
+This directory persists MASQ repositories for reuse.
+Files her are not intended for manual edits.
+If anything is crewed up, you may delete directories here (under `.src/`).
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -10,24 +10,37 @@ BUILD_TARGET_PATH=~/builds/${BUILD_DIR}
 mkdir -p $BUILD_TARGET_PATH
 rm -r ${BUILD_TARGET_PATH}/*
 # Download MASQ Node source
-git clone --depth 1 https://github.com/MASQ-Project/Node.git src
+if [ -d ~/.src/Node ]; then
+	cd ~/.src/Node
+    
+    # discrad any local change
+    git clean -fd
+    git reset --hard origin/master
+
+    # keep repo updated
+    git pull
+else
+	cd ~/.src
+    git clone https://github.com/MASQ-Project/Node.git
+fi
+cp -r ~/.src ~/src
 # Compile MASQNode
-cd ~/src/node
+cd ~/src/Node/node
 cargo build --release --verbose
-cp ~/src/node/target/${RUST_CROSS_TARGET}/release/MASQNode ${BUILD_TARGET_PATH}/
+cp ~/src/Node/node/target/${RUST_CROSS_TARGET}/release/MASQNode ${BUILD_TARGET_PATH}/
 # Compile masq
-cd ~/src/masq
+cd ~/src/Node/masq
 cargo build --release --verbose
-cp ~/src/node/target/${RUST_CROSS_TARGET}/release/masq ${BUILD_TARGET_PATH}/
+cp ~/src/Node/node/target/${RUST_CROSS_TARGET}/release/masq ${BUILD_TARGET_PATH}/
 # Compile dns_utility
-cd ~/src/dns_utility
+cd ~/src/Node/dns_utility
 cargo build --release --verbose
-cp ~/src/dns_utility/target/${RUST_CROSS_TARGET}/release/dns_utility ${BUILD_TARGET_PATH}/
+cp ~/src/Node/dns_utility/target/${RUST_CROSS_TARGET}/release/dns_utility ${BUILD_TARGET_PATH}/
 # Compile port_exposer
-cd ~/src/port_exposer
+cd ~/src/Node/port_exposer
 cargo build --release --verbose
-cp ~/src/port_exposer/target/${RUST_CROSS_TARGET}/release/port_exposer ${BUILD_TARGET_PATH}/
+cp ~/src/Node/port_exposer/target/${RUST_CROSS_TARGET}/release/port_exposer ${BUILD_TARGET_PATH}/
 # Compile automap
-cd ~/src/automap
+cd ~/src/Node/automap
 cargo build --release --verbose
-cp ~/src/automap/target/${RUST_CROSS_TARGET}/release/automap ${BUILD_TARGET_PATH}/
+cp ~/src/Node/automap/target/${RUST_CROSS_TARGET}/release/automap ${BUILD_TARGET_PATH}/
