598c5af4301a865559be194846295e7804b57603
microoo <hu@microoo.net>
2021-03-16 15:45:03 +0000

    amend

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -35,17 +35,17 @@ else
 	CROSS_MODE=build
 fi
 # Compile MASQNode
-cargo build --manifest-path node/Cargo.toml --release --verbose
+cargo $CROSS_MODE --manifest-path node/Cargo.toml --release --verbose
 cp node/target/${RUST_CROSS_TARGET}/release/MASQNode ${BUILD_TARGET_PATH}/
 # Compile masq
-cargo build --manifest-path masq/Cargo.toml --release --verbose
+cargo $CROSS_MODE --manifest-path masq/Cargo.toml --release --verbose
 cp node/target/${RUST_CROSS_TARGET}/release/masq ${BUILD_TARGET_PATH}/
 # Compile dns_utility
-cargo build --manifest-path dns_utility/Cargo.toml --release --verbose
+cargo $CROSS_MODE --manifest-path dns_utility/Cargo.toml --release --verbose
 cp dns_utility/target/${RUST_CROSS_TARGET}/release/dns_utility ${BUILD_TARGET_PATH}/
 # Compile port_exposer
-cargo build --manifest-path port_exposer/Cargo.toml --release --verbose
+cargo $CROSS_MODE --manifest-path port_exposer/Cargo.toml --release --verbose
 cp port_exposer/target/${RUST_CROSS_TARGET}/release/port_exposer ${BUILD_TARGET_PATH}/
 # Compile automap
-cargo build --manifest-path automap/Cargo.toml --release --verbose
+cargo $CROSS_MODE --manifest-path automap/Cargo.toml --release --verbose
 cp automap/target/${RUST_CROSS_TARGET}/release/automap ${BUILD_TARGET_PATH}/
